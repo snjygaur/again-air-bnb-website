6e7b21867bfb5446e79de159e1af8efa35890ea6
Harsh Vardhan Gaur <160375906+snjygaur@users.noreply.github.com>
2026-09-10 01:57:30 +0530

    Handle listings and reviews without owners safely

--- middleware.js
+++ middleware.js
@@ -25,10 +25,14 @@ module.exports.saveRedirectUrl = (req,res,next) =>{
 module.exports.isOwner = async(req,res,next) =>{
     let {id} = req.params;
    let listing = await Listing.findById(id);
-  if(!listing.owner.equals(res.locals.currUser._id)){
+   if(!listing){
+    req.flash("error","listing does not exist");
+    return res.redirect("/listings");
+   }
+   if(!listing.owner || !res.locals.currUser || !listing.owner.equals(res.locals.currUser._id)){
     req.flash("error","you are not the owner of the listing");
     return res.redirect(`/listings/${id}`);
-  }
+   }
   next();
   
 }
@@ -36,7 +40,7 @@ module.exports.isOwner = async(req,res,next) =>{
 module.exports.isReviewAuthor = async(req,res,next) =>{
     let {reviewId,id} = req.params;
     let review = await Review.findById(reviewId);
-  if(!review.author.equals(res.locals.currUser._id)){
+    if(!review || !review.author || !res.locals.currUser || !review.author.equals(res.locals.currUser._id)){
     req.flash("error","you are not author of this review");
     return res.redirect(`/listings/${id}`);
   }
